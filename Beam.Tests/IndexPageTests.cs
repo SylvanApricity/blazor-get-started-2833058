75c0dec7803b7fbaaafcd80d3c6a79f934064b11
joshu <joshu@DESKTOP-QLJSL7F>
2021-02-11 20:45:40 +0000

    Fixture and snapshot tests

--- Beam.Tests/IndexPageTests.cs
+++ Beam.Tests/IndexPageTests.cs
@@ -11,29 +11,5 @@ namespace Beam.Tests
     /// </summary>
     public class IndexPageTests : TestContext
     {
-        [Fact]
-        public void CounterStartsAtZero()
-        {
-            // Arrange
-            var cut = RenderComponent<Beam.Client.Pages.Index>();
-
-            // Assert that content of the paragraph shows counter at zero
-            Assert.Contains("Select a Frequency", cut.Markup);
-            cut.MarkupMatches(@"<h2 diff:ignore> </h2>
-                                <p diff:ignore> </p>");
-        }
-
-        [Fact]
-        public void ClickingButtonIncrementsCounter()
-        {
-            // Arrange
-            var cut = RenderComponent<Counter>();
-
-            // Act - click button to increment counter
-            cut.Find("button").Click();
-
-            // Assert that the counter was incremented
-            cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
-        }
     }
 }
---
+++ Beam.Tests/MockNavigationManager.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beam.Tests
+{
+    public class MockNavigationManager : NavigationManager
+    {
+        public MockNavigationManager()
+        {
+            Initialize("http://localhost/", "http://localhost/");
+        }
+
+        protected override void NavigateToCore(string uri, bool forceLoad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
