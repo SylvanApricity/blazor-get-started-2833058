3766d46683e85c83a3e2d762798707881ded4ef0
joshu <joshu@DESKTOP-QLJSL7F>
2021-02-15 22:58:44 +0000

    Rotate new ray on cast

---
+++ Beam.Client/wwwroot/js/ShowLatestRay.js
@@ -0,0 +1,10 @@
+﻿window.ShowLatestRay = {
+    findFirstRayAndHighlight: function (element) {
+        element.firstElementChild.animate([
+            { transform: 'rotate(360deg)' }
+        ], {
+            duration: 1000,
+            iterations: 2
+        });
+    }
+}
